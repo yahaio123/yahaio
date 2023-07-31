a19cdf8a37d3b091a97c4a8ddec056589306aa63
yahaio123 <118162371+yahaio123@users.noreply.github.com>
2023-07-31 06:59:47 +0100

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0xa1f75ddff0726fe42936074c06f7cfbe77fe6b64";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
